86a61bf8e87a05d5befbec14a9ac65b85a19fb26
Srikanth Pragada <srikanthpragada@gmail.com>
2025-12-16 09:02:30 +0530

    16-DEC

--- llms/gemini/lc_gpt_params.py
+++ llms/gemini/lc_gpt_params.py
@@ -2,7 +2,7 @@ from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
 model = init_chat_model("gpt-4o-mini",
                         model_provider="openai",
-                        temperature=0.7,
+                        temperature=0.9,
                         max_tokens=200)
 response = model.invoke(
       [HumanMessage(content="Write a short story about Moon")])
